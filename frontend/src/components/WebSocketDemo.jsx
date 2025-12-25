d5f8de6b9f4436b64eafa0f4cc307a3acc9010ef
mithun <mithunpoojary180@gmail.com>
2025-12-25 21:23:41 +0530

    made interactive ui

--- frontend/src/components/WebSocketDemo.jsx
+++ frontend/src/components/WebSocketDemo.jsx
@@ -1,56 +1,92 @@
-import { useCallback, useEffect, useState } from "react";
+import { useCallback, useEffect, useRef, useState } from "react";
 import "./styles.css";
 
 export default function WebSocketDemo() {
   const [socket, setSocket] = useState(null);
-  const [messages, setMessages] = useState([
-    { isUser: true, mssg: "What's up" },
-    { isUser: false, mssg: "Fine" },
-    { isUser: true, mssg: "What's up" },
-  ]);
-  const [text, setText] = useState("");
+  const [messages, setMessages] = useState([]);
   const [aiReply, setAiReply] = useState("");
+  const [isAiTyping, setIsAiTyping] = useState(false);
+
+  const inputRef = useRef(null);
+  const replyRef = useRef("");
+  const scrollRef = useRef(null);
+  const bufferRef = useRef("");
 
   useEffect(() => {
     const ws = new WebSocket("ws://localhost:8080");
+    inputRef.current.focus();
     setSocket(ws);
 
+    // typing interval . renders 1 char in 30ms .
+    const typingInterval = setInterval(() => {
+      if (bufferRef.current?.length > 0) {
+        const nextChar = bufferRef.current.charAt(0);
+        bufferRef.current = bufferRef.current.substring(1);
+        setAiReply((prev) => prev + nextChar);
+      }
+    }, 10);
+
     ws.onmessage = (event) => {
-      console.log("event", event);
       if (event.data === "[END]") {
-        console.log("here", aiReply);
-        setMessages((prev) => [...prev, { isUser: false, mssg: aiReply }]);
-        setAiReply("");
+        //if all buffered is rendered then only set the message
+        const checkDone = setInterval(() => {
+          if (bufferRef.current.length === 0) {
+            const aiMessage = replyRef.current;
+            if (aiMessage) {
+              setMessages((prev) => [
+                ...prev,
+                { isUser: false, mssg: aiMessage },
+              ]);
+            }
+            replyRef.current = "";
+            bufferRef.current = "";
+            inputRef.current.focus();
+            setAiReply("");
+            setIsAiTyping(false);
+            clearInterval(checkDone);
+          }
+        }, 100);
       } else {
-        setAiReply((prev) => prev + event.data);
+        replyRef.current = replyRef.current + event.data;
+        bufferRef.current = bufferRef.current + event.data;
       }
     };
 
     return () => {
       ws.close();
+      clearInterval(typingInterval);
     };
-  }, [aiReply]);
+  }, []);
+
+  // scrolling to bottom of the message when message or aireply changes
+  useEffect(() => {
+    if (scrollRef.current) {
+      scrollRef.current.scrollIntoView({ behavior: "smooth" });
+    }
+  }, [messages, aiReply]);
 
   const handleKeyDown = (e) => {
-    if (e.key === "Enter") {
+    if (!isAiTyping && !e.shiftKey && e.key === "Enter") {
       e.preventDefault();
       sendMessage();
     }
   };
 
   const sendMessage = () => {
-    if (socket && text.trim()) {
-      setMessages((prev) => [...prev, { isUser: true, mssg: text.trim() }]);
-      socket.send(text);
-      setText("");
+    const userMessage = inputRef.current.value.trim();
+    if (socket && userMessage) {
+      setMessages((prev) => [...prev, { isUser: true, mssg: userMessage }]);
+      socket.send(userMessage);
+      inputRef.current.value = "";
+      setIsAiTyping(true);
     }
   };
 
   return (
-    <div style={{ padding: 20 }}>
-      <h2>React WebSocket Chat</h2>
+    <div className="chatWindow">
+      <h2 style={{ padding: "16px" }}>React WebSocket Chat</h2>
 
-      <div>
+      <div className="messageContainer">
         {messages.map((msg, i) => (
           <div
             key={i}
@@ -65,25 +101,21 @@ export default function WebSocketDemo() {
           </div>
         ))}
         <div className="messageBoxLeft">
-          {aiReply && (
-            <p className="aiReply">
-               {aiReply}▌
-            </p> // typing effect
+          {isAiTyping && (
+            <p className="message aiReply aiTyping">{aiReply}</p> // typing effect
           )}
         </div>
+        <div ref={scrollRef} style={{ height: "1px" }}></div>
       </div>
 
-      <div style={{ marginBottom: 10 }}>
-        <input
+      <div className="inputBoxContainer">
+        <textarea
           id="textMssg"
-          type="text-area"
+          ref={inputRef}
           rows={4}
-          value={text}
-          onChange={(e) => setText(e.target.value)}
           placeholder="Type message"
           onKeyDown={handleKeyDown}
           className="inputBox"
-          disabled={aiReply !== ""}
         />
       </div>
     </div>
